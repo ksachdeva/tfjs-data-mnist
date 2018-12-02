be3380f65694fb9a1e08c85f516fa30400358a81
Kapil Sachdeva <ksachdeva17@gmail.com>
2018-12-02 11:40:46 -0600

    feat - update example to display the images in the canvas

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,20 +1,50 @@
+import * as tfjs from '@tensorflow/tfjs-core';
+import * as tfd from '@tensorflow/tfjs-data';
+
 import {MNISTDataset} from '../../src';
 
+let ds: MNISTDataset = null;
+
+async function toPixels(sample: tfd.DataElement, element: HTMLCanvasElement) {
+  const reshapedTensor = sample[0].reshape([28, 28, 1]);
+  await tfjs.toPixels(reshapedTensor, element);
+}
+
 async function onBodyLoad() {
-  const ds = await MNISTDataset.create();
-  const tensors = await ds.testDataset.iterator();
+  document.getElementById('shuffledButton')
+      .addEventListener('click', onShuffleImages);
 
-  const val = await tensors.next();
-  console.log(val);
-  console.log(await tensors.next());
+  ds = await MNISTDataset.create();
 
-  const ds2 = ds.testDataset.batch(2);
+  // we take the first 5 elements
+  const only5 = await ds.testDataset.take(5).iterator();
 
-  const tensors2 = await ds2.iterator();
+  let n = await only5.next();
+  let i = 1;
 
-  const val2 = await tensors2.next();
-  console.log(val2);
-  console.log(await tensors2.next());
+  do {
+    await toPixels(
+        n.value, document.getElementById('canvas' + i) as HTMLCanvasElement);
+    n = await only5.next();
+    i = i + 1;
+  } while (!n.done);
+
+  onShuffleImages();
+}
+
+async function onShuffleImages() {
+  // we take shuffle the dataset and then take 5 elements
+  const shuffled5 = await ds.testDataset.shuffle(10).take(5).iterator();
+
+  let n = await shuffled5.next();
+  let i = 6;
+
+  do {
+    await toPixels(
+        n.value, document.getElementById('canvas' + i) as HTMLCanvasElement);
+    n = await shuffled5.next();
+    i = i + 1;
+  } while (!n.done);
 }
 
 document.addEventListener('DOMContentLoaded', onBodyLoad, false);
