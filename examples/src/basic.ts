feea20bf27849348e841701d7bc351869e4fad3b
Kapil Sachdeva <ksachdeva17@gmail.com>
2018-12-10 17:35:03 -0600

    fix - adjust to reflect the changes in the dataset class

--- examples/src/basic.ts
+++ examples/src/basic.ts
@@ -4,9 +4,9 @@ import {MNISTDataset} from '../../src';
 
 let ds: MNISTDataset = null;
 
-async function toPixels(sample: tf.data.DataElement, element: HTMLCanvasElement) {
-  const reshapedTensor = sample[0].reshape([28, 28, 1]);
-  await tf.toPixels(reshapedTensor, element);
+async function toPixels(sample: number[][], element: HTMLCanvasElement) {
+  const reshapedTensor = tf.tensor(sample[0]).reshape([28, 28, 1]);
+  await tf.toPixels(reshapedTensor as tf.Tensor2D, element);
 }
 
 async function onBodyLoad() {
@@ -16,13 +16,23 @@ async function onBodyLoad() {
   ds = await MNISTDataset.create();
 
   // we take the first 5 elements
-  const only5 = await ds.testDataset.take(5).iterator();
+  const only5 = await ds.testDataset
+                    .map((row: Array<{[key: string]: number}>) => {
+                      const [rawFeatures, rawLabel] = row;
+                      const convertedFeatures = Object.values(rawFeatures);
+                      const convertedLabel = Object.values(rawLabel);
+                      return [convertedFeatures, convertedLabel];
+                    })
+                    .take(5)
+                    .iterator();
 
   let n = await only5.next();
   let i = 1;
 
-  const feature = n.value[0] as tf.Tensor;
-  const label = n.value[1] as tf.Tensor;
+  console.log(n);
+
+  const feature = tf.tensor(n.value[0]);
+  const label = tf.tensor(n.value[1]);
 
   document.getElementById('feature-shape').innerText = feature.shape.toString();
   document.getElementById('label-shape').innerText = label.shape.toString();
@@ -39,7 +49,16 @@ async function onBodyLoad() {
 
 async function onShuffleImages() {
   // we take shuffle the dataset and then take 5 elements
-  const shuffled5 = await ds.testDataset.shuffle(10).take(5).iterator();
+  const shuffled5 = await ds.testDataset
+                        .map((row: Array<{[key: string]: number}>) => {
+                          const [rawFeatures, rawLabel] = row;
+                          const convertedFeatures = Object.values(rawFeatures);
+                          const convertedLabel = Object.values(rawLabel);
+                          return [convertedFeatures, convertedLabel];
+                        })
+                        .shuffle(10)
+                        .take(5)
+                        .iterator();
 
   let n = await shuffled5.next();
   let i = 6;
